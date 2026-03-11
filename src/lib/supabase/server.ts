d59cb47bb3e433f24ed17b0e778c2876e249dfff
sudharsan8621 <sudharsan8621@gmail.com>
2026-03-11 20:43:50 +0530

    fix createClient

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,10 +1,8 @@
-'use client'
-
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export function createClient() {
-  return createSupabaseClient(supabaseUrl, supabaseAnonKey)
+export async function createClient() {
+return createSupabaseClient(supabaseUrl, supabaseAnonKey)
 }
